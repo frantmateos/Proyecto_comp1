d071dac732d98d2c7384abb468bc7cba2f9a9fb4
frantmateos <frantmateos@gmail.com>
2024-06-10 14:35:27 -0300

    ,

--- clase.s
+++ clase.s
@@ -1,22 +1,35 @@
--text
+.text
+
+
+
 
 -global_autof
 autof:
-    PUSH (R4,R5,R6,LR)
-    MDV R4,#15
-    LDR R5, #array // load base address of array into R5
-    
+    PUSH (R4,R5,R6,LR)          //Guarda los registros R4, R5, R6 y el enlace de retorno (LR) en la pila.
+    MOV R4,#15                  //; Carga el valor 15 en el registro R4.
+    LDR R5, #array              //Carga la dirección base del array en el registro R5. 
 
-loop: LDRB R6, [R5],#1
-      MDV R0,R6
-      BL disp_binary
-      MVN R0,R6 
-      BL leds
-      MOV R0, #300
-      BL delayMillis
-      SUBS R4, R4 #1
-      BNE loop;
-      POP(R4, R5, R6, PC) // return from autof
+loop: LDRB R6, [R5],#1           ;Carga un byte del array en R6 y auto-incrementa R5.
+      MOV R0,R6                  ;Carga el valor de R6 en R0. De nuevo
+      BL disp_binary             ; Llama a la subrutina disp_binary con R0 como argumento.
+      MVN R0,R6                  ; Mueve el complemento bit a bit de R6 a R0.
+      BL leds                    ; Llama a la subrutina leds con R0 como argumento.
+      MOV R0, #300               ; Carga el valor 300 en R0.
+      BL delayMillis             ; Llama a la subrutina delayMillis con R0 como argumento.
+      SUBS R4, R4 #1             ; Resta 1 de R4 y actualiza las banderas.
+      BNE loop;                  ; Si R4 no es cero, salta de nuevo a la etiqueta 'loop'.
+      POP(R4, R5, R6, PC)        // return from autof,  ; Restaura los registros y regresa de la subrutina.
+
+Este es el bucle principal que itera a través del array. Para cada byte del array:
+
+Lo carga en R6.
+Llama a disp_binary para mostrar el valor en binario.
+Llama a leds con el complemento bit a bit del valor.
+Espera 300 milisegundos.
+Decrementa el contador de bucles (R4).
+Repite hasta que R4 sea 0.
+
+
 .data
 array:
 .byte 0x80
@@ -37,3 +50,7 @@ array:
 
 
 .end
+Esta sección define un array de bytes con valores que probablemente representan estados para los LEDs.
+El objetivo principal de este código es crear un "jueguito" de LEDs donde los patrones de encendido y apagado de
+ los LEDs cambian en función de los valores almacenados en el array. Cada byte del array representa un estado específico 
+de los LEDs, y el bucle recorre estos estados, aplicándolos a los LEDs y mostrando sus complementos.
--- proyecto.cpp
+++ proyecto.cpp
@@ -2,21 +2,32 @@
 //pi&gcc -c autof.s
 //pi%gcc prj -o autof.o -o prjfinal 
 //pi%sudo ./prjfinal 
+#include <string>
+#include <cstdlib>
+#include <conio.h> // getch
+#include <vector>
 
+#define ENTER 13
+#define BACKSPACE 8
+#define INTENTOS 3
+
+using namespace std;
 
 #include <stdio.h>
 #include<conio.h>
 #include<unistd.h>
 #include <iostream>
-//#include "EasyPIO.h"
+#include "EasyPIO.h"
 
 int menu(void);
 void autof(void);
 void disp_binary(int);
 void not_yet(void);
 void choque(void);
+void sirena(void);
+void pendulo_newton(void);
 
-void paquete(void);
+void retardo(unsigned long int a);
 
 //const char led[]={14,15,18,25,24,6,7};
 
@@ -29,23 +40,100 @@ int main (void) {
     leds(0xFF); // Turn off leds active low
 
 */
-    
-    int choice;
-    for( ; ; ){
+    vector<string> usuarios;
+    vector<string> claves;
+
+    usuarios.push_back("tomi");
+    usuarios.push_back("pipe");
+    usuarios.push_back("fran");
+
+    claves.push_back("123");
+    claves.push_back("456");
+    claves.push_back("789");
+
+    string usuario, password;
+
+    int contador = 0;
+    bool ingresa = false;
+
+    do
+    {
+        system("cls");
+        cout << "\t\t\tLOGIN DE USUARIO" << endl;
+        cout << "\t\t\t----------------" << endl;
+        cout << "\n\tUsuario: ";
+        getline(cin, usuario);
+
+        char caracter;
+
+        cout << "\tPassword: ";
+        caracter = getch();
+
+        password = "";
+
+        while (caracter != ENTER)
+        {
+
+            if (caracter != BACKSPACE)
+            {
+                password.push_back(caracter);
+                cout << "*";
+            }
+            else
+            {
+                if (password.length() > 0)
+                {
+                    cout << "\b \b";
+                    password = password.substr(0, password.length() - 1);
+                }
+            }
+
+            caracter = getch();
+        }
+
+        for (int i = 0; i < usuarios.size(); i++)
+        {
+            if (usuarios[i] == usuario && claves[i] == password)
+            {
+                ingresa = true;
+                break;
+            }
+        }
+
+        if (!ingresa)
+        {
+            cout << "\n\n\tEl usuario y/o password son incorrectos" << endl;
+            cin.get();
+            contador++;
+        }
+
+    } while (ingresa == false && contador < INTENTOS);
+
+    if (ingresa == false)
+    {
+        cout << "\n\tUsted no pudo ingresar al sistema. ADIOS" << endl;
+    }
+    else
+    {
+        cout << "\n\n\tBienvenido al sistema" << endl;
+        int choice;
+         for( ; ; ){
         choice = menu();
         switch(choice){
             case 1: autof();
             break;
             case 2: choque();
             break;
-            case 3: paquete();
+            case 3: pendulo_newton();
             break; 
-
-            case 4: not_yet();
+            case 4: sirena();
             break; 
             case 5: return(-1);
         }
     }
+    }
+    cin.get();
+
 }
 int menu(void){
     int s;
@@ -53,8 +141,8 @@ int menu(void){
         printf("\n Menu \n");
         printf("\n OPCION 1: Auto Fantastico \n");
         printf("\n OPCION 2: Choque \n");
-        printf("\n OPCION 3: Paquete \n");
-        printf("\n OPCION 4: ..\n");
+        printf("\n OPCION 3: Pendulo de Newton \n");
+        printf("\n OPCION 4: Sirena \n");
         printf("\n OPCION 5: salir\n");
         scanf("%d", &s);
     }while(s<1|| s>5 );
@@ -71,7 +159,7 @@ void disp_binary(int i){
             printf("*");
         }
         else{
-            printf("_");
+            printf("-");
         }
     }
     printf("\n");
@@ -113,43 +201,38 @@ void choque(void){
     }
 }
 
-void paquete(void) {
-    printf("Paquete..\n");
+void sirena(void){
+    printf("Sirena...\n");
+    int j=0;
+    //unsigned char tabla[] = {0xF0, 0xF};
+    unsigned char tabla[8] = {0xF2, 0x12, 0x12, 0xFe, 0x90, 0x90, 0x9E, 0x00};
+    do{
+       for(int i=0; i<8; i++){
+       disp_binary(tabla[i]);
+       sleep(1);
+     }
+     j--; 
+    }while(j>=0);
     
-    // Define los patrones de LEDs para la animación.
-    unsigned char tabla[] = {
-        0x03, // 00000011: dos LEDs encendidos desde la izquierda
-        0x03, // 00000011: dos LEDs encendidos desde la izquierda
-        0x03, // 00000011: dos LEDs encendidos desde la izquierda
-        0x81, // 10000001: un LED en cada extremo
-        0x42, // 01000010: un LED en cada extremo más cerca del centro
-        0x24, // 00100100: un LED en cada extremo más cerca del centro
-        0x18, // 00011000: LEDs centrales (chocan)
-        0x1C, // 00011100: LEDs centrales, el LED del centro recibe los LEDs de la izquierda
-        0x0E, // 00001110: LEDs se desplazan hacia la derecha
-        0x07, // 00000111: LEDs se desplazan hacia la derecha
-        0x03, // 00000011: LEDs se desplazan hacia la derecha
-        0x01, // 00000001: Un LED en el extremo derecho
-        0x00  // 00000000: Apagar todos los LEDs
-    };
+}
+
+void pendulo_newton(void) {
+    printf("Pendulo de Newton..\n");
+    int j=2;
+
+    int tabla[8] = {0x3C,0x3A,0x39,0x3A,0x3C,0x5C,0x9C,0x5C}; 
+
+    do{
+        for(int i = 0; i < 8; i++) {
+           disp_binary(tabla[i]);
+           sleep(1);
+        }
+        j--;
+    }while(j>=0);
+}
+
+void retardo(unsigned long int a){
+    while (a)
+    a--;
     
-    // Iterar sobre la tabla y mostrar cada patrón con un retraso.
-    for(int i = 0; i < sizeof(tabla) / sizeof(tabla[0]); i++) {
-        disp_binary(tabla[i]);
-        sleep(1); // Espera 1 segundo entre cada cambio de patrón.
-    }
 }
-
-// Ejemplo de función para mostrar los bits (necesitas implementar tu propia función)
-void disp_binary(unsigned char byte) {
-    for (int i = 7; i >= 0; i--) {
-        putchar((byte & (1 << i)) ? '1' : '0');
-    }
-    putchar('\n');
-}
-
-
-void not_yet(void){
-    printf("not yet");
-}
-
